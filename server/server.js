a215a675639295ddfe77fbf8688e498bff3dcbc6
Avner <avner.h@nanox.vision>
2022-10-30 19:10:45 +0200

    mock server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const jsonServer = require("json-server");
+const middleware = jsonServer.defaults();
+const server = jsonServer.create();
+
+server.use(middleware);
+server.use(jsonServer.bodyParser);
+const userData = require("../server/data/users");
+server.get("/api/users", (req, res) => {
+  res.status(200).send(userData.getUsers);
+});
+
+server.listen(3000, () => {
+  console.log(`Server listening on localhost:3000`);
+});
