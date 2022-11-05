d352c9bb73867e6847d8d7c5e41ec47a74b99eab
Avner <avner.h@nanox.vision>
2022-11-06 00:43:48 +0200

    upgrade

---
+++ src/app/store/from-reducer-basic.ts
@@ -0,0 +1,21 @@
+import { BehaviorSubject, Observable, scan, share, Subject } from 'rxjs';
+
+type Reducer<S, T> = (state: S, event: T) => S;
+
+type FromReducerReturnType<T, S> = [Observable<S>, (event: T) => void];
+
+export function fromReducer<S, T>(
+  reducer: Reducer<S, T>,
+  initialState: S
+): FromReducerReturnType<T, S> {
+  const events$ = new Subject<T>();
+
+  const state$ = events$.pipe(
+    scan(reducer, initialState),
+    share({ connector: () => new BehaviorSubject(initialState) })
+  );
+
+  const dispatch = (event: T) => events$.next(event);
+
+  return [state$, dispatch];
+}
